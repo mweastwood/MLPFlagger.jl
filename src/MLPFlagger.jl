ca5f58fa89da9e00b641fcf8cf7d788ca8c79178
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-02-18 15:07:04 -0800

    Add routines for flagging antennas and clearing flags

--- src/MLPFlagger.jl
+++ src/MLPFlagger.jl
@@ -15,5 +15,87 @@
 
 module MLPFlagger
 
+using CasaCore.Tables
+
+@doc """
+Clear all of the flags in the measurement set.
+""" ->
+function clear!(ms::Table)
+    N = numrows(ms)
+    Nfreq = length(freq(ms))
+    flags = zeros(Bool,4,Nfreq,N)
+    ms["FLAG"] = flags
+    flags
+end
+
+@doc """
+Derive and apply antenna flags to the measurement set.
+""" ->
+function antennas(ms::Table)
+    autos = getautos(ms)
+
+    # Flag the antennas with too much power
+    xx_power = log10(abs(squeeze(median(squeeze(autos[1,:,:],1),1),1)))
+    xy_power = log10(abs(squeeze(median(squeeze(autos[2,:,:],1),1),1)))
+    yy_power = log10(abs(squeeze(median(squeeze(autos[4,:,:],1),1),1)))
+
+    xx_flags = flag(xx_power,Niter=2)
+    xy_flags = flag(xy_power,Niter=2)
+    yy_flags = flag(yy_power,Niter=2)
+
+    # Flag the antenna if appears in at least 2 of 3 cases
+    votes = xx_flags + xy_flags + yy_flags
+    flagged = votes .>= 2
+
+    # Apply the flags to the measurement set
+    ant1 = ms["ANTENNA1"] + 1
+    ant2 = ms["ANTENNA2"] + 1
+    flags = ms["FLAG"]
+    for α = 1:N
+        if flagged[ant1[α]] || flagged[ant2[α]]
+            flags[:,:,α] = true
+        end
+    end
+    ms["FLAG"] = flags
+
+    flags
+end
+
+function getautos(ms)
+    ant1 = ms["ANTENNA1"] + 1
+    ant2 = ms["ANTENNA2"] + 1
+    data = ms["DATA"]
+
+    Nant  = length(unique(ant1))
+    Nfreq = size(data,2)
+    Nbase = size(data,3)
+
+    autos = zeros(4,Nfreq,Nant)
+    count = 1
+    for α = 1:Nbase
+        if ant1[α] == ant2[α]
+            autos[:,:,count] = real(data[:,:,α])
+            count += 1
+        end
+    end
+    autos
+end
+
+function flag(vector;Niter::Int=1)
+    flags = zeros(Bool,length(vector))
+    for i = 1:Niter
+        flag!(flags,vector)
+    end
+    flags
+end
+
+function flag!(flags,vector)
+    m0 = median(vector[!flags])
+    m2 = median((vector[!flags]-m0).^2)
+    stddev   = sqrt(m2)
+    flags[:] = abs(vector-m0) .> 5stddev
+    flags
+end
+
 end
 
